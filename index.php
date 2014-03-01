ac21217abe1a9ee958b1b4dad110ce521c09241d
jallarzie <jallarzie@gmail.com>
2014-03-01 09:44:51 -0500

    Setup jquery mobile

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-
-<?php
-require_once 'DB.php';
-require_once 'employmentController.php';
-
-$emp = new EmploymentController();
-
-?>
-
-
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Single page template</title>
+	<link rel="stylesheet" href="/css/jquery.mobile-1.4.2.min.css">
+	<script src="https://www.google.com/jsapi"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="/js/jquery.mobile-1.4.2.min.js"></script>
+</head>
+
+<body>
+
+<div data-role="page">
+
+	<div data-role="header">
+		<h1>Single page</h1>
+	</div><!-- /header -->
+
+	<div role="main" class="ui-content">
+		
+	</div><!-- /content -->
+
+	<div data-role="footer">
+		<h4>Footer content</h4>
+	</div><!-- /footer -->
+
+</div><!-- /page -->
+
+</body>
 </html>

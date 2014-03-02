82a496eda47818924a872d1f3015ac1610303346
unknown <Sluong@SLUONG.immersion.com>
2014-03-02 15:30:31 -0500

    Added themes

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Single page template</title>
 	<link rel="stylesheet" href="/css/jquery.mobile-1.4.2.min.css">
+	<link rel="stylesheet" href="themes/blue.min.css" />
 	<script src="https://www.google.com/jsapi"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="/js/jquery.mobile-1.4.2.min.js"></script>
